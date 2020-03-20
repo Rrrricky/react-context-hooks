ad04b19f978de95b7a40708c57620de03734420d
Rrrricky <eric.dufreche@hetic.net>
2020-03-20 11:10:15 +0100

    :tada: Git init

---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class BookList extends React.Component {
+  render() {
+    return (
+      <div className="book-list">
+        <ul>
+          <li>The LightHouse</li>
+          <li>Harry Potter</li>
+          <li>Hunger Games</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default BookList
