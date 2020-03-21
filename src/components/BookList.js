65b696fb4864d81c518616205a3830b342f8e4c4
Rrrricky <eric.dufreche@hetic.net>
2020-03-21 23:40:58 +0100

    Replace contextType by context.Consumer

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,18 +2,23 @@ import React from 'react'
 import { ThemeContext } from "../contexts/ThemeContext"
 
 class BookList extends React.Component {
-  static contextType = ThemeContext // Set this static property to...
+
   render() {
-    const { isLightTheme, light, dark } = this.context // ...get values though this.context
-    const theme = isLightTheme ? light : dark
     return (
-      <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
-        <ul>
-          <li style={{ background: theme.ui }}>The Lighthouse</li>
-          <li style={{ background: theme.ui }}>Harry Potter</li>
-          <li style={{ background: theme.ui }}>Hunger Games</li>
-        </ul>
-      </div>
+      // .Consumer gives access to context
+      <ThemeContext.Consumer>{(context) => {
+        const { isLightTheme, light, dark } = context // ...get values though this.context
+        const theme = isLightTheme ? light : dark
+        return (
+          <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
+            <ul>
+              <li style={{ background: theme.ui }}>The Lighthouse</li>
+              <li style={{ background: theme.ui }}>Harry Potter</li>
+              <li style={{ background: theme.ui }}>Hunger Games</li>
+            </ul>
+          </div>
+        )
+      }}</ThemeContext.Consumer>
     )
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,23 @@ import React from 'react'
 import { ThemeContext } from "../contexts/ThemeContext"
 
 class Navbar extends React.Component {
-  static contextType = ThemeContext // Use contextType for passing data to this component (go up in the tree to get the first provider)
   render() {
-    const {isLightTheme, light, dark} = this.context
-    const theme = isLightTheme ? light : dark
     return (
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Dev</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>{(context) => {
+        const {isLightTheme, light, dark} = context
+        const theme = isLightTheme ? light : dark
+        return (
+          <nav style={{ background: theme.ui, color: theme.syntax }}>
+            <h1>Context App</h1>
+            <ul>
+              <li>Dev</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        )
+      }}
+      </ThemeContext.Consumer>
     )
   }
 }
