31560fd4cb8d452f89e89f0bdea7a01a52a767b7
Rrrricky <eric.dufreche@hetic.net>
2020-03-21 22:55:39 +0100

    Use contextType to consume context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { ThemeContext } from "../contexts/ThemeContext"
 
 class BookList extends React.Component {
+  static contextType = ThemeContext // Set this static property to...
   render() {
+    const { isLightTheme, light, dark } = this.context // ...get values though this.context
+    const theme = isLightTheme ? light : dark
     return (
-      <div className="book-list">
+      <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
         <ul>
-          <li>The Lighthouse</li>
-          <li>Harry Potter</li>
-          <li>Hunger Games</li>
+          <li style={{ background: theme.ui }}>The Lighthouse</li>
+          <li style={{ background: theme.ui }}>Harry Potter</li>
+          <li style={{ background: theme.ui }}>Hunger Games</li>
         </ul>
       </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { ThemeContext } from "../contexts/ThemeContext"
 
 class Navbar extends React.Component {
+  static contextType = ThemeContext // Use contextType for passing data to this component (go up in the tree to get the first provider)
   render() {
+    const {isLightTheme, light, dark} = this.context
+    const theme = isLightTheme ? light : dark
     return (
-      <nav>
+      <nav style={{ background: theme.ui, color: theme.syntax }}>
         <h1>Context App</h1>
         <ul>
           <li>Dev</li>
